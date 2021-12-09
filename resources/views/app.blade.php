a05c4b38342d468557e9e76b66d663d3434f0fa2
Eduardo2203967 <2203967@my.ipleiria.pt>
2021-12-09 17:00:01 +0000

    App Commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     
+    
     @yield('styles')
 </head>
 
