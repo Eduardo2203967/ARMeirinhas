45706d2486fa9ccfe6202beea3ae16667dc0157c
Eduardo2203967 <2203967@my.ipleiria.pt>
2021-12-09 16:40:22 +0000

    jfkhbew

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -34,10 +34,11 @@
 </div>
 
 @include('layout.partials.footer')
+<script src="{{ asset('js/popper.min.js') }}"></script>
 <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
 <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('js/popper.min.js') }}"></script>
+
 </body>
 
 </html>
