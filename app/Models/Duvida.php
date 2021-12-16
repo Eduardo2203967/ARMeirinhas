c811abe2e4575f6528a856cc9d6b1b375cdf4974
Eduardo2203967 <2203967@my.ipleiria.pt>
2021-12-16 11:59:10 +0000

    duvidas update

--- app/Models/Duvida.php
+++ app/Models/Duvida.php
@@ -8,15 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Duvida extends Model
 {
     use HasFactory;
-
+    public $fillable = ['nome_duv', 'email_duv', 'msg_duv'];
     /**
      * The attributes that are mass assignable.
      *
      * @var string[]
      */
-    protected $fillable = [
-        'nome_duv',
-        'email_duv',
-        'msg_duv',
-    ];
 }
+
+
+
+        
